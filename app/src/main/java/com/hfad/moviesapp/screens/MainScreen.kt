99184e828c0deb6f7d8c1e8473560afc4161fb66
Vasiliy Brave <vskh93.009@gmail.com>
2022-05-05 09:57:23 +0300

    - разметка экрана SplashScreen посредством Compose.

--- app/src/main/java/com/hfad/moviesapp/screens/MainScreen.kt
+++ app/src/main/java/com/hfad/moviesapp/screens/MainScreen.kt
@@ -1,18 +1,24 @@
 package com.hfad.moviesapp.screens
 
+import android.text.style.BackgroundColorSpan
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hfad.moviesapp.navigation.Screens
+import com.hfad.moviesapp.ui.theme.MoviesAppTheme
 
 @Composable
 fun MainScreen() {
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(text = "Test")
-        
-    }
-    
 }
+
--- app/src/main/java/com/hfad/moviesapp/screens/SplashScreen.kt
+++ app/src/main/java/com/hfad/moviesapp/screens/SplashScreen.kt
@@ -1,10 +1,44 @@
 package com.hfad.moviesapp.screens
 
-
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
-
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hfad.moviesapp.ui.theme.MoviesAppTheme
 
 @Composable
 fun SplashScreen() {
 
 }
+
+@Composable
+fun Splash() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            modifier = Modifier.size(120.dp),
+            imageVector = Icons.Default.PlayArrow,
+            contentDescription = "",
+            tint = Color.Black
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun prevSplash() {
+    MoviesAppTheme {
+        Splash()
+    }
+}
+
