0a9c9f11774e3abe9480463c5618519a382eca77
Vasiliy Brave <vskh93.009@gmail.com>
2022-05-07 13:19:55 +0300

    - преобразование html текста в обычный текст.

--- app/src/main/java/com/hfad/moviesapp/screens/DetailsScreen.kt
+++ app/src/main/java/com/hfad/moviesapp/screens/DetailsScreen.kt
@@ -3,6 +3,8 @@ package com.hfad.moviesapp.screens
 import android.security.identity.AccessControlProfileId
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.magnifier
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +18,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 import com.hfad.moviesapp.MainViewModel
+import com.hfad.moviesapp.utils.htmlText
 
 @Composable
 fun DetailsScreen(viewModel: MainViewModel, itemId: String) {
@@ -28,20 +31,57 @@ fun DetailsScreen(viewModel: MainViewModel, itemId: String) {
             .fillMaxSize()
             .padding(vertical = 24.dp, horizontal = 8.dp)
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = rememberImagePainter(currentItem?.image?.medium),
-                contentDescription = null,
-                modifier = Modifier.size(512.dp)
-            )
-            Text(
-                text = currentItem?.name ?: "",
-                fontWeight = FontWeight.Bold,
-                fontSize = 32.sp,
-                modifier = Modifier.padding(top = 16.dp)
-            )
+        LazyColumn {
+           item{
+               Column(
+                   horizontalAlignment = Alignment.CenterHorizontally
+               ) {
+                   Image(
+                       painter = rememberImagePainter(currentItem?.image?.medium),
+                       contentDescription = null,
+                       modifier = Modifier.size(512.dp)
+                   )
+                   Text(
+                       text = currentItem?.name ?: "",
+                       fontWeight = FontWeight.Bold,
+                       fontSize = 32.sp,
+                       modifier = Modifier.padding(top = 16.dp)
+                   )
+                   Row(
+                       modifier = Modifier.padding(top = 8.dp)
+                   ) {
+                       Text(
+                           text = "Rating: ",
+                           fontWeight = FontWeight.Bold,
+                           fontSize = 18.sp
+                       )
+
+                       Text(
+                           text = currentItem?.rating?.average.toString(),
+                           fontSize = 18.sp
+                       )
+                   }
+                   Row(
+                       modifier = Modifier.padding(top = 8.dp)
+                   ) {
+                       Text(
+                           text = "Genre: ",
+                           fontWeight = FontWeight.Bold,
+                           fontSize = 18.sp
+                       )
+                       currentItem?.genres?.take(2)?.forEach {
+                           Text(
+                               text = " {$it} ",
+                               fontSize = 18.sp
+                           )
+                       }
+                   }
+
+                       htmlText  (
+                           html = currentItem?.summary ?: "",
+                       modifier = Modifier.padding(top = 10.dp))
+               }
+           }
         }
     }
 }
