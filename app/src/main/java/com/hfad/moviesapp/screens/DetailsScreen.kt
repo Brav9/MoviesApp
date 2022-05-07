de6250fa77ec6c01fbc364e7a7f0b86f5e461d25
Vasiliy Brave <vskh93.009@gmail.com>
2022-05-07 12:56:24 +0300

    - при нажатии на карточку осуществили переход на экран деталей, с показом постера фильма и его названия.

--- app/src/main/java/com/hfad/moviesapp/screens/DetailsScreen.kt
+++ app/src/main/java/com/hfad/moviesapp/screens/DetailsScreen.kt
@@ -1,12 +1,47 @@
 package com.hfad.moviesapp.screens
 
 import android.security.identity.AccessControlProfileId
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentComposer
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.hfad.moviesapp.MainViewModel
 
 @Composable
-fun DetailsScreen(navController: NavController, viewModel: MainViewModel, itemId: String) {
-    Text("Details screen: item id: ${itemId}")
+fun DetailsScreen(viewModel: MainViewModel, itemId: String) {
+    val currentItem = viewModel.allMovies
+        .observeAsState(listOf()).value
+        .firstOrNull { it.id == itemId.toInt() }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 24.dp, horizontal = 8.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = rememberImagePainter(currentItem?.image?.medium),
+                contentDescription = null,
+                modifier = Modifier.size(512.dp)
+            )
+            Text(
+                text = currentItem?.name ?: "",
+                fontWeight = FontWeight.Bold,
+                fontSize = 32.sp,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
+    }
 }
